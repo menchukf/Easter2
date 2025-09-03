98533994d2af9154d9d8501c2748d844f7d85a98
Fedor Menchukov <77518360+menchukf@users.noreply.github.com>
2025-09-03 18:18:45 +0000

    initial commit

--- Easter.java
+++ Easter.java
@@ -1,1 +1,8 @@
+// Your name
+// This program will calculate the date easter falls on given a year after 1583.
+public class Easter{
+    public static void main(String[] args) {
+        //code goes here
+    }
 
+}
